d0186b35d808fbcee1e2af920233f06f9fe7459b
marvinpatrick <122240303+marvinpatrick@users.noreply.github.com>
2024-07-21 09:36:19 -0400

    Now that I have Snyk to scan for vulnerabilities, I feel confident using third party libraries. In newer projects I prefer to build security in it.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -26,6 +26,19 @@ kotlin {
                 implementation(compose.material)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
+
+                //For Multiplatform ViewModel
+                api("com.rickclephas.kmm:kmm-viewmodel-core:1.0.0-ALPHA-21")
+
+                //For Multiplatform Dependency Injection
+                implementation("io.insert-koin:koin-core:3.5.6")
+                implementation("io.insert-koin:koin-compose:1.1.5")
+
+                //For HTTP Requests
+                implementation("io.ktor:ktor-client-core:2.3.12")
+                implementation("io.ktor:ktor-client-logging:2.3.12")
+                implementation("io.ktor:ktor-client-content-negotiation:2.3.12")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.12")
             }
         }
         val androidMain by getting {
